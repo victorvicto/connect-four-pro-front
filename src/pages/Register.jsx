2657a924beb2fc9aa1f653d4588284e4f58d2ad6
DEMUYSERE Victor <victor.demu@gmail.com>
2023-05-23 17:41:02 +0200

    minor changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,7 +20,7 @@ function Register() {
             <h2>Create Account</h2>
             {errorMessage.length>0 && <ErrorPannel errorMessage={errorMessage}/>}
             <input type="email" placeholder="Email" name="email" required />
-            <input type="text" placeholder="Username" name="username" required />
+            <input type="text" placeholder="Username" name="username" onChange={(e) => {setUsername(e.target.value)}} required />
             <input type="password" placeholder="Password" name="password" required />
             <input type="password" placeholder="Confirm Password" name="passwordconf" required />
             <button type="submit" className='fat-btn'>
