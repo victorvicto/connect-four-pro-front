946fbbaa0540c855e9c8e3930849a2813723b122
victorvicto <victor.demu@gmail.com>
2024-11-25 22:39:15 +0100

    Implement matchmaking logic for finding and joining games based on player ELO

--- api/socketManagement/socketGames.js
+++ api/socketManagement/socketGames.js
@@ -13,6 +13,38 @@ function initializeSocket(server) {
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
 
+        socket.on('findGame', (playerInfo) => {
+            const { playerId, elo } = playerInfo;
+            let bestMatch = null;
+            let bestMatchIndex = -1;
+            let minEloDifference = Infinity;
+    
+            // Find the best match in the pool
+            for (let i = 0; i < matchmakingPool.length; i++) {
+                const opponent = matchmakingPool[i];
+                const eloDifference = Math.abs(opponent.elo - elo);
+                if (eloDifference < minEloDifference) {
+                    minEloDifference = eloDifference;
+                    bestMatch = opponent;
+                    bestMatchIndex = i;
+                }
+            }
+    
+            if (bestMatch) {
+                // Remove the matched player from the pool
+                matchmakingPool.splice(bestMatchIndex, 1);
+    
+                // Create a new game
+                const gameId = `game-${Date.now()}`;
+                socket.join(gameId);
+                io.to(bestMatch.socketId).emit('gameFound', { gameId, opponent: playerInfo });
+                socket.emit('gameFound', { gameId, opponent: bestMatch });
+            } else {
+                // Add the player to the pool
+                matchmakingPool.push({ socketId: socket.id, playerId, elo });
+            }
+        });
+
         socket.on('joinGame', (gameId) => {
             socket.join(gameId);
             console.log(`User ${socket.id} joined game ${gameId}`);
