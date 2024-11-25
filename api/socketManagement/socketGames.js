2dfa647f7376ae86ec1c54dd05d254b22ecea063
victorvicto <victor.demu@gmail.com>
2024-11-26 00:00:48 +0100

    Refactor matchmaking logic to retrieve player ELO from database and streamline game finding process

--- api/socketManagement/socketGames.js
+++ api/socketManagement/socketGames.js
@@ -5,6 +5,8 @@ const MongoStore = require('connect-mongo');
 const User = require('../models/users'); // Adjust the path as necessary
 
 function initializeSocket(server) {
+    const matchmakingPool = []; // TODO: make a database collection for this
+
     const io = new Server(server, {
         cors: {
             origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
@@ -35,38 +37,49 @@ function initializeSocket(server) {
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
 
-        socket.on('findGame', (playerInfo) => {
+        socket.on('findGame', () => {
             if (socket.request.user) {
                 const user = socket.request.user;
-                const { playerId, elo } = playerInfo;
-                let bestMatch = null;
-                let bestMatchIndex = -1;
-                let minEloDifference = Infinity;
-
-                // Find the best match in the pool
-                for (let i = 0; i < matchmakingPool.length; i++) {
-                    const opponent = matchmakingPool[i];
-                    const eloDifference = Math.abs(opponent.elo - elo);
-                    if (eloDifference < minEloDifference) {
-                        minEloDifference = eloDifference;
-                        bestMatch = opponent;
-                        bestMatchIndex = i;
+                User.findById(user._id, (err, playerInfo) => {
+                    if (err) {
+                        console.error('Error retrieving user info:', err);
+                        return;
                     }
-                }
+                    if (!playerInfo) {
+                        console.error('User not found');
+                        return;
+                    }
+                    const elo = playerInfo.elo;
+                    const playerId = playerInfo._id;
+                    let bestMatch = null;
+                    let bestMatchIndex = -1;
+                    let minEloDifference = Infinity;
 
-                if (bestMatch) {
-                    // Remove the matched player from the pool
-                    matchmakingPool.splice(bestMatchIndex, 1);
+                    // Find the best match in the pool
+                    for (let i = 0; i < matchmakingPool.length; i++) {
+                        const opponent = matchmakingPool[i];
+                        const eloDifference = Math.abs(opponent.elo - elo);
+                        if (eloDifference < minEloDifference) {
+                            minEloDifference = eloDifference;
+                            bestMatch = opponent;
+                            bestMatchIndex = i;
+                        }
+                    }
 
-                    // Create a new game
-                    const gameId = `game-${Date.now()}`;
-                    socket.join(gameId);
-                    io.to(bestMatch.socketId).emit('gameFound', { gameId, opponent: playerInfo });
-                    socket.emit('gameFound', { gameId, opponent: bestMatch });
-                } else {
-                    // Add the player to the pool
-                    matchmakingPool.push({ socketId: socket.id, playerId, elo });
-                }
+                    if (bestMatch) {
+                        // Remove the matched player from the pool
+                        matchmakingPool.splice(bestMatchIndex, 1);
+
+                        // Create a new game
+                        const gameId = `game-${Date.now()}`;
+                        socket.join(gameId);
+                        io.to(bestMatch.socketId).emit('gameFound', { gameId, opponent: playerInfo });
+                        socket.emit('gameFound', { gameId, opponent: bestMatch });
+                    } else {
+                        // Add the player to the pool
+                        matchmakingPool.push({ socketId: socket.id, playerId, elo });
+                    }
+                });
             } else {
                 console.log('User not authenticated');
             }
